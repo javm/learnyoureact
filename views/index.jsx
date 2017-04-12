842df2bfc660d7df98b0da4939c9c6d5d6c89406
javm <quetzal1910@gmail.com>
2017-04-11 23:34:56 -0500

    Challenge 3 PROPS completed

--- views/index.jsx
+++ views/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-export default class TodoBox extends React.Component{
-   render() {
-     return (<div className="todoBox">
-       <h1>Todos</h1>
-       <TodoList />
-       <TodoForm />
-       </div>);
+
+export default class TodoBox extends React.Component {
+    
+     render() {
+	 return (<div className="todoBox">
+	     <h1>Todos</h1>
+	     <TodoList />
+	     <TodoForm />
+	     </div>);
      }
-  }
+}
 
-
+/*
 class TodoList extends React.Component {
       // Write code here
      render(){
@@ -18,12 +20,42 @@ class TodoList extends React.Component {
     </div>);
     }
 }
+*/
+
+class TodoList extends React.Component{
+    render() {
+	return (
+	    <div className="todoList">
+            <table style={{border: "2px solid black"}}>
+            <tbody>
+            <Todo title="Shopping">Milk</Todo>
+            <Todo title="Hair cut">13:00</Todo>
+            </tbody>
+            </table>
+            </div>
+	);
+    }
+}
+
+
+class Todo extends React.Component {
+      // Write code here
+     render(){
+	 return (
+	     <tr>
+             <td style={{border:"1px solid black"}}>{this.props.title}</td>
+             <td style={{border:"1px solid black"}}>{this.props.children}</td>
+             </tr>
+	 );
+    }
+}
+
 
 class TodoForm extends React.Component {
-      // Write code here
-      render(){
-        return(<div class="todoForm">
-          I am a TodoForm.
+
+    render(){
+        return(<div className="todoForm">
+            I am a TodoForm.
         </div>);
-     }
+    }
 }
